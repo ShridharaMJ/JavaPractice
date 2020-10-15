2e90c0544cd69dc7d8d4d0c6d55f5c005ee78bef
Lenovo <Lenovo@ShridharaMJ>
2020-10-16 00:18:07 +0530

    Testing

---
+++ src/com/java/expetion/ExceptionDemo.java
@@ -0,0 +1,15 @@
+package com.java.expetion;
+
+import org.testng.annotations.Test;
+
+public class ExceptionDemo {
+
+	@Test
+	public void exception() {
+		
+		System.out.println(10/0);
+		
+		
+	}
+	
+}
